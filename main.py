c8fd15ae503220e78c3a4148fe57bd8985683248
George Zografos <george.zografos@dazn.com>
2019-04-09 10:00:29 +0200

    Add Python script to copy DynamoDB table data from src to dest

---
+++ main.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import argparse
+import os
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-src', help='source table name', required=True)
+parser.add_argument('-dest', help='destination table name', required=True)
+
+args = parser.parse_args()
+
+
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+REGION_NAME = os.environ.get('AWS_DEFAULT_REGION', 'eu-central-1')
+SOURCE_TABLE_NAME = args.src
+DESTINATION_TABLE_NAME = args.dest
+
+client = boto3.client('dynamodb', region_name=REGION_NAME)
+
+last_evaluated_key = True
+
+while last_evaluated_key:
+    if type(last_evaluated_key) == bool:
+        response = client.scan(TableName=SOURCE_TABLE_NAME)
+    else:
+        response = client.scan(TableName=SOURCE_TABLE_NAME,
+                               ExclusiveStartKey=last_evaluated_key)
+
+    chunk_idx = 0
+    total_items = 0
+    for chunk in chunks(response['Items'], 25):
+        total_items += len(chunk)
+        chunk_idx += 1
+
+        batch_write = {DESTINATION_TABLE_NAME: []}
+
+        for item in chunk:
+            batch_write_item = {'PutRequest': {'Item': {}}}
+
+            for (k, v) in item.items():
+                batch_write_item['PutRequest']['Item'][k] = v
+
+            batch_write[DESTINATION_TABLE_NAME].append(batch_write_item)
+        response = client.batch_write_item(RequestItems=batch_write)
+
+        print('Written chunk: {}'.format(chunk_idx))
+    last_evaluated_key = response.get('LastEvalutedKey', None)
+
+print('Total items copied: {}'.format(total_items))
